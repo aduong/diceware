2e0d44e2e7462a2498301318281ff2e176c10ef3
Adrian Duong <adrian.duong@gmail.com>
2016-07-25 15:27:24 -0400

    Make prettier. Add separator support

--- generate.scala
+++ generate.scala
@@ -5,28 +5,57 @@ import scala.util.{Try,Random,Either}
 
 case class Arguments(
   numWords: Int,
-  noNumbers: Boolean = false,
-  noSymbols: Boolean = false,
-  strong: Boolean = false)
+  noNumbers: Boolean,
+  noSymbols: Boolean,
+  strong: Boolean,
+  separator: String
+)
 
 val FileName = "diceware.wordlist"
 val NumFormat = NumberFormat.getInstance
 
+val UsageStr = """Usage: [OPTIONS] LENGTH
+                 |Options:
+                 |  --help            displays this message
+                 |  --no-symbols      omits symbols from the diceware list
+                 |  --no-numbers      omits numbers from the diceware list
+                 |  --separator SEP   string to use as separator between words
+                 |  --strong          uses a stronger crypto-secure PRNG (may block)""".stripMargin
+
 def noExclusion[A]: (A => Boolean) = _ => false
 
-def readArguments(args: Array[String]): Either[String, Arguments] = {
-  for {
-    numWords <- args.find(_.forall(_.isDigit))
-      .map(_.toInt)
-      .toRight("Password length missing. It must be one of the arguments")
-      .right
-  } yield Arguments(
-    numWords,
-    noNumbers = args.indexOf("--no-numbers") >= 0,
-    noSymbols = args.indexOf("--no-symbols") >= 0,
-    strong = args.indexOf("--strong") >= 0
-  )
+def isInt(str: String): Boolean = Try(str.toInt).map(_ => true).getOrElse(false)
+
+def isSymbol(str: String): Boolean = !str.forall(_.isLetterOrDigit)
+
+def readNumWords(args: Array[String]): Either[String, Int] =
+  args.find(isInt).map(_.toInt)
+    .toRight("Error: password length missing.\n" + UsageStr)
+
+def readSeparator(args: Array[String]): Either[String, Option[String]] = {
+  val i = args.indexOf("--separator")
+  if (i < 0) Right(None)
+  else {
+    val separatorIdx = i + 1
+    if (separatorIdx >= args.size) Left("\"--separator\" present but missing separator argument.\n" + UsageStr)
+    else Right(Some(args(separatorIdx)))
+  }
 }
+
+def readArguments(args: Array[String]): Either[String, Arguments] =
+  if (args.indexOf("--help") >= 0) Left(UsageStr)
+  else {
+    for {
+      numWords <- readNumWords(args).right
+      separatorOpt <- readSeparator(args).right
+    } yield Arguments(
+      numWords,
+      noNumbers = args.indexOf("--no-numbers") >= 0,
+      noSymbols = args.indexOf("--no-symbols") >= 0,
+      strong = args.indexOf("--strong") >= 0,
+      separator = separatorOpt.getOrElse(" ")
+    )
+  }
 
 def readDicewareListFromFile(source: Source): Seq[String] =
   source.getLines.map(_.split("""\s+""")(1)).toIndexedSeq
@@ -39,10 +68,6 @@ def randStream(end: Int, strong: Boolean = false): Stream[Int] = {
   Stream.continually(rand.nextInt(end))
 }
 
-def isInt(str: String): Boolean = Try(str.toInt).map(_ => true).getOrElse(false)
-
-def isSymbol(str: String): Boolean = !str.forall(_.isLetterOrDigit)
-
 def log(str: => String): Unit = {
   System.err.println(str)
 }
@@ -52,7 +77,7 @@ readArguments(args) match {
     log(errorStr)
     System.exit(1)
 
-  case Right(Arguments(numWords, noNumbers, noSymbols, strong)) =>
+  case Right(Arguments(numWords, noNumbers, noSymbols, strong, separator)) =>
     val numberExcluder: (String => Boolean) =
       if (noNumbers) s => isInt(s)
       else noExclusion
@@ -91,5 +116,11 @@ readArguments(args) match {
       log("Using a strong PRNG... this may take some time...")
     }
 
-    randStream(wordList.size, strong).map(wordList).take(numWords).foreach(println)
+    log("")
+
+    println(
+      randStream(wordList.size, strong)
+        .map(wordList)
+        .take(numWords)
+        .mkString(separator))
 }
